9072f128ca05f26923128f15257bdc871936918c
You <you9009@foxmail.com>
2020-05-29 16:25:08 +0800

    修改作业2

--- fed-e-task-01-02/code/app.js
+++ fed-e-task-01-02/code/app.js
@@ -6,11 +6,11 @@ const { Maybe, Container } = require('./support')
 // horsepower 马力,dollar_value 价格, in_stock 库存
 const cars = [
 	{ name: 'Ferrari FF', horsepower: 660, dollar_value: 700000, in_stock: true },
-	{ name: ' Spyker C12 Zagato', horsepower: 650, dollar_value: 648000, in_stock: false },
+	{ name: 'Spyker C12 Zagato', horsepower: 650, dollar_value: 648000, in_stock: false },
 	{ name: 'Jaguar XKR-S', horsepower: 550, dollar_value: 132000, in_stock: false },
 	{ name: 'Audi R8', horsepower: 525, dollar_value: 114200, in_stock: false },
 	{ name: 'Aston Martin One-77', horsepower: 750, dollar_value: 1850000, in_stock: true },
-	{ name: ' Pagani Huayra', horsepower: 700, dollar_value: 1300000, in_stock: false }
+	{ name: 'Pagani Huayra', horsepower: 700, dollar_value: 1300000, in_stock: false }
 ]
 
 // 练习1：使用函数组合 fp.flowRight() 重新实现下面这个函数
