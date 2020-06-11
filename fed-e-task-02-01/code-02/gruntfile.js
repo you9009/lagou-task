e1d1ffedda98fda16a6e69f91eca5bfb34063ce5
You <you9009@foxmail.com>
2020-06-11 11:28:01 +0800

    修改code3

--- fed-e-task-02-01/code-02/gruntfile.js
+++ fed-e-task-02-01/code-02/gruntfile.js
@@ -1,4 +1,154 @@
 // 实现这个项目的构建任务
-module.exports = grunt => {
 
+const loadGruntTasks = require('load-grunt-tasks')
+
+module.exports = function(grunt) {
+	'use strict'
+	const config = {
+		app: 'src', // 源文件目录
+		dest: 'dest' // 目标文件目录
+	}
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		// 清空
+		clean: {
+			temp: '<%= config.dest %>/**'
+		},
+		csslint: {
+			// 检查 CSS 语法
+			src: [ '<%= config.app %>/**/*.css' ]
+		},
+		sass: {
+			options: {
+				implementation: sass
+			},
+			main: {
+				files: {
+					'<%= config.dest %>/styles/main.css': '<%= config.app %>/**/*.scss'
+				}
+			}
+		},
+		cssmin: {
+			options: {
+				keepSpecialComments: 0, // 移除 CSS 文件中的所有注释
+				report: 'gzip'
+			},
+			minify: {
+				expand: true,
+				cwd: '<%= config.app %>/assets/styles',
+				src: [ 'global.css' ],
+				dest: '<%= config.dest %>/styles/'
+			}
+		},
+		jshint: {
+			files: [ '<%= config.app %>/**/*.js' ],
+			options: {
+				curly: true,
+				eqeqeq: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				boss: true,
+				node: true,
+				globals: {
+					exports: true,
+					jQuery: true,
+					console: true,
+					module: true
+				}
+			}
+		},
+		imagemin: {
+			// /压缩优化图片大小
+			dist: {
+				options: {
+					optimizationLevel: 3
+				},
+				files: [
+					{
+						expand: true,
+						cwd: '<%= config.app %>/assets/',
+						src: [ '**/*.{png,jpg,jpeg}' ], // 优化 img 目录下所有 png/jpg/jpeg 图片
+						dest: '<%= config.dest %>/assets/images/' // 优化后的图片保存位置，默认覆盖
+					}
+				]
+			}
+		},
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: [ '<%= config.app %>/**/*.js' ],
+				dest: '<%= config.dest %>/<%= pkg.name %>.js'
+			}
+		},
+		uglify: {
+			options: {
+				//为true表示允许添加头部信息
+				stripBanners: true,
+				//在头部添加 js文件名和时间的注释
+				banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+			},
+			dist: {
+				files: {
+					'<%= config.dest %>/<%= pkg.name %>.min.js': [ '<%= concat.dist.dest %>' ]
+				}
+			}
+		},
+		htmlmin: {
+      //html 压缩
+			options: {
+				removeComments: true,
+				removeCommentsFromCDATA: true,
+				collapseWhitespace: true,
+				collapseBooleanAttributes: true,
+				removeAttributeQuotes: true,
+				removeRedundantAttributes: true,
+				useShortDoctype: true,
+				removeEmptyAttributes: true,
+				removeOptionalTags: true
+			},
+			build: {
+				expand: true,
+				cwd: '<%= config.app %>/',
+				src: [ '*.html' ],
+				dest: '<%= config.dest %>/'
+			}
+		},
+		copy: {
+			font: {
+				expand: true,
+				cwd: '<%= config.app %>/assets/font/',
+				src: [ '**' ],
+				dest: '<%= config.dest %>/font/'
+			},
+			images: {
+				expand: true,
+				cwd: '<%= config.app %>/assets/images/',
+				src: [ '*.svg' ],
+				dest: '<%= config.dest %>/images/'
+			}
+		},
+		watch: {
+			files: [ '<%= jshint.files %>' ],
+			tasks: [ 'jshint' ]
+		}
+	})
+	// 加载所有的grunt插件中的任务
+	loadGruntTasks(grunt)
+	// 定义默认任务
+	grunt.registerTask('default', [
+		'clean',
+		'csslint',
+		'sass',
+		'cssmin',
+		'jshint',
+		'concat',
+		'uglify',
+		'imagemin',
+		'htmlmin',
+		'watch'
+	])
 }
--- fed-e-task-02-01/code-03/gulpfile.js
+++ fed-e-task-02-01/code-03/gulpfile.js
@@ -1,18 +1,15 @@
-// 实现这个项目的构建任务
 const path = require('path')
-const { src, dest, parallel, series, watch } = require('gulp')
+const gulp = require('gulp')
 
 const del = require('del')
-const browserSync = require('browser-sync')
-const loadPlugins = require('gulp-load-plugins')
-const minimist = require('minimist')
 const Comb = require('csscomb')
-const standard = require('standard')
 const autoprefixer = require('autoprefixer')
+const cssnano = require('cssnano')
+const standard = require('standard')
+const browserSync = require('browser-sync')
 
-const plugins = loadPlugins()
-const bs = browserSync.create()
-const argv = minimist(process.argv.slice(2))
+const gulpLoadPlugins = require('gulp-load-plugins')
+const minimist = require('minimist')
 
 const data = {
 	menus: [
@@ -20,10 +17,6 @@ const data = {
 			name: 'Home',
 			icon: 'aperture',
 			link: 'index.html'
-		},
-		{
-			name: 'Features',
-			link: 'features.html'
 		},
 		{
 			name: 'About',
@@ -55,126 +48,168 @@ const data = {
 	date: new Date()
 }
 
+const config = {
+	src: 'src',
+	dest: 'dist',
+	public: 'public',
+	temp: 'temp',
+	paths: {
+		pages: '**/*.html',
+		styles: 'assets/styles/**/*.scss',
+		scripts: 'assets/scripts/**/*.js',
+		images: 'assets/images/**/*.{jpg,jpeg,png,gif,svg}',
+		fonts: 'assets/fonts/**/*.{eot,svg,ttf,woff,woff2}'
+	}
+}
+
+const Plugins = gulpLoadPlugins()
+const bs = browserSync.create()
+const argv = minimist(process.argv.slice(2))
+const isProd = process.env.NODE_ENV ? process.env.NODE_ENV === 'production' : argv.production || argv.prod || false
+
 const clean = () => {
-	return del([ 'dist', 'temp' ])
+	return del([ config.temp, config.dest ])
 }
 
 const lint = (done) => {
 	const comb = new Comb(require('./.csscomb.json'))
-	comb.processPath('src')
-	const cwd = path.join(__dirname, 'src')
-	standard.lintFiles('src/assets/scripts/*.js', { cwd, fix: true }, done)
+	comb.processPath(config.src)
+	const cwd = path.join(__dirname, config.src)
+	standard.lintFiles(config.paths.scripts, { cwd, fix: true }, done)
 }
 
 const style = () => {
-	return src('src/assets/styles/*.scss', { base: 'src' })
-		.pipe(plugins.sass({ outputStyle: 'expanded' }))
-		.pipe(plugins.postcss(autoprefixer()))
-		.pipe(dest('temp'))
+	return gulp
+		.src(config.paths.styles, { cwd: config.src, base: config.src, sourcemaps: !isProd })
+		.pipe(Plugins.plumber({ errorHandler: Plugins.sass.logError }))
+		.pipe(Plugins.sass.sync({ outputStyle: 'expanded', precision: 10, includePaths: [ '.' ] }))
+		.pipe(Plugins.postcss([ autoprefixer() ]))
+		.pipe(gulp.dest(config.temp, { sourcemaps: '.' }))
 		.pipe(bs.reload({ stream: true }))
 }
 
 const script = () => {
-	return src('src/assets/scripts/*.js', { base: 'src' })
-		.pipe(plugins.babel({ presets: [ '@babel/preset-env' ] }))
-		.pipe(dest('temp'))
+	return gulp
+		.src(config.paths.scripts, { cwd: config.src, base: config.src, sourcemaps: !isProd })
+		.pipe(Plugins.plumber())
+		.pipe(Plugins.babel({ presets: [ '@babel/preset-env' ] }))
+		.pipe(gulp.dest(config.temp, { sourcemaps: '.' }))
 		.pipe(bs.reload({ stream: true }))
 }
 
 const page = () => {
-	return src('src/*.html', { base: 'src' })
-		.pipe(plugins.swig({ data }))
-		.pipe(dest('temp'))
-		.pipe(bs.reload({ stream: true }))
-}
-
-const image = () => {
-	return src('src/assets/images/**', { base: 'src' }).pipe(plugins.imagemin()).pipe(dest('dist'))
-}
-
-const font = () => {
-	return src('src/assets/fonts/**', { base: 'src' }).pipe(plugins.imagemin()).pipe(dest('dist'))
-}
-
-const extra = () => {
-	return src('public/**', { base: 'public' }).pipe(dest('dist'))
-}
-
-const serve = () => {
-	watch('src/assets/styles/*.scss', style)
-	watch('src/assets/scripts/*.js', script)
-	watch('src/*.html', page)
-	// watch('src/assets/images/**', image)
-	// watch('src/assets/fonts/**', font)
-	// watch('public/**', extra)
-	watch([ 'src/assets/images/**', 'src/assets/fonts/**', 'public/**' ], bs.reload)
-
-	bs.init({
-		notify: false,
-		port: 1234,
-		// files: 'dist/**',
-		server: {
-			baseDir: [ 'temp', 'src', 'public' ],
-			routes: {
-				'/node_modules': 'node_modules'
-			}
-		}
-	})
+	return gulp
+		.src(config.paths.pages, { cwd: config.src, base: config.src, ignore: [ '{layouts,partials}/**' ] })
+		.pipe(Plugins.plumber())
+		.pipe(Plugins.swig({ defaults: { cache: false, locals: data } }))
+		.pipe(gulp.dest(config.temp))
 }
 
 const useref = () => {
-	return src('temp/*.html', { base: 'temp' })
-		.pipe(plugins.useref({ searchPath: [ 'temp', '.' ] }))
-		.pipe(plugins.if(/\.js$/, plugins.uglify()))
-		.pipe(plugins.if(/\.css$/, plugins.cleanCss()))
+	const beautifyOpts = { indent_size: 2, max_preserve_newlines: 1 }
+	const uglifyOpts = { compress: { drop_console: true } }
+	const postcssOpts = [ cssnano({ safe: true, autoprefixer: false }) ]
+	const htmlminOpts = {
+		collapseWhitespace: true,
+		minifyCSS: true,
+		minifyJS: true,
+		processConditionalComments: true,
+		removeComments: true,
+		removeEmptyAttributes: true,
+		removeScriptTypeAttributes: true,
+		removeStyleLinkTypeAttributes: true
+	}
+
+	return gulp
+		.src(config.paths.pages, { cwd: config.temp, base: config.temp })
+		.pipe(Plugins.plumber())
+		.pipe(Plugins.useref({ searchPath: [ '.', '..' ] }))
+		.pipe(Plugins.if(/\.js$/, Plugins.if(isProd, Plugins.uglify(uglifyOpts), Plugins.beautify.js(beautifyOpts))))
 		.pipe(
-			plugins.if(
-				/\.html$/,
-				plugins.htmlmin({
-					collapseWhitespace: true,
-					minifyCSS: true,
-					minifyJS: true
-				})
-			)
+			Plugins.if(/\.css$/, Plugins.if(isProd, Plugins.postcss(postcssOpts), Plugins.beautify.css(beautifyOpts)))
 		)
-		.pipe(dest('dist'))
+		.pipe(
+			Plugins.if(/\.html$/, Plugins.if(isProd, Plugins.htmlmin(htmlminOpts), Plugins.beautify.html(beautifyOpts)))
+		)
+		.pipe(gulp.dest(config.dest))
+}
+
+const image = () => {
+	return gulp
+		.src(config.paths.images, { cwd: config.src, base: config.src, since: gulp.lastRun(image) })
+		.pipe(Plugins.plumber())
+		.pipe(Plugins.if(isProd, Plugins.imagemin()))
+		.pipe(gulp.dest(config.dest))
+}
+
+const font = () => {
+	return gulp
+		.src(config.paths.fonts, { cwd: config.src, base: config.src })
+		.pipe(Plugins.plumber())
+		.pipe(Plugins.if(isProd, Plugins.imagemin()))
+		.pipe(gulp.dest(config.dest))
+}
+
+const extra = () => {
+	return gulp.src('**', { cwd: config.public, base: config.public, dot: true }).pipe(gulp.dest(config.dest))
+}
+
+const measure = () => {
+	return gulp
+		.src('**', { cwd: config.dest })
+		.pipe(Plugins.plumber())
+		.pipe(Plugins.size({ title: `${isProd ? 'Prodcuction' : 'Development'} mode build`, gzip: true }))
+}
+
+const upload = () => {
+	return gulp
+		.src('**', { cwd: config.dest })
+		.pipe(Plugins.babel({ presets: [ '@babel/preset-env' ] }))
+		.pipe(Plugins.plumber())
+		.pipe(
+			Plugins.ghPages({
+				cacheDir: `${config.temp}/publish`,
+				branch: argv.branch === undefined ? 'gh-pages' : argv.branch
+			})
+		)
+}
+
+const devServer = () => {
+	gulp.watch(config.paths.styles, { cwd: config.src }, style)
+	gulp.watch(config.paths.scripts, { cwd: config.src }, script)
+	gulp.watch(config.paths.pages, { cwd: config.src }, page)
+	gulp.watch([ config.paths.images, config.paths.fonts ], { cwd: config.src }, bs.reload)
+	gulp.watch('**', { cwd: config.public }, bs.reload)
+
+	bs.init({
+		notify: false,
+		port: argv.port === undefined ? 1234 : argv.port,
+		open: argv.open === undefined ? false : argv.open,
+		plugins: [ `bs-html-injector?files[]=${config.temp}/*.html` ],
+		server: {
+			baseDir: [ config.temp, config.src, config.public ],
+			routes: { '/node_modules': 'node_modules' }
+		}
+	})
 }
 
 const distServer = () => {
 	bs.init({
 		notify: false,
-		port: argv.port === undefined ? 2080 : argv.port,
+		port: argv.port === undefined ? 1234 : argv.port,
 		open: argv.open === undefined ? false : argv.open,
-		server: 'dist'
+		server: config.dest
 	})
 }
 
-const measure = () => {
-	return src('**', { cwd: config.dest })
-		.pipe(plugins.plumber())
-		.pipe(plugins.size({ title: `${isProd ? 'Prodcuction' : 'Development'} mode build`, gzip: true }))
-}
+const compile = gulp.parallel(style, script, page)
 
-const upload = () => {
-	return src('**', { cwd: config.dest })
-		.pipe(plugins.plumber())
-		.pipe(
-			plugins.ghPages({ cacheDir: `temp/publish`, branch: argv.branch === undefined ? 'gh-pages' : argv.branch })
-		)
-}
+const serve = gulp.series(compile, devServer)
 
-const compile = parallel(style, script, page)
-const build = series(clean, parallel(series(compile, useref), image, font, extra), measure)
-const develop = series(compile, serve)
-const start = series(build, distServer)
-const deploy = series(build, upload)
+const build = gulp.series(clean, gulp.parallel(gulp.series(compile, useref), image, font, extra), measure)
 
-module.exports = {
-	clean,
-	lint,
-	serve,
-	build,
-	develop,
-	start,
-	deploy
-}
+const start = gulp.series(build, distServer)
+
+const deploy = gulp.series(build, upload)
+
+module.exports = { clean, lint, compile, serve, build, start, deploy }
